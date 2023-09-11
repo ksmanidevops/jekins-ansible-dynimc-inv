cf1a79666cd91cd81b2099f5b24ba53b58b4cb2e
ksmanidevops <73011264+ksmanidevops@users.noreply.github.com>
2023-09-12 03:13:39 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ region = "eu-north-1"
 }
 
 resource "aws_instance" "AWSServer"{
-ami = "ami-0da59f1af71ea4ad2"
+ami = "ami-0c147c2e2b026f094"
 instance_type = "t3.micro"
 key_name = "redhatdevops"
 security_groups = ["launch-wizard-9"]
