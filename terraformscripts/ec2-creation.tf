eeafa559b9a801c6723f7a0cd219fdeaa5f0877b
ksmanidevops <73011264+ksmanidevops@users.noreply.github.com>
2023-09-12 03:00:57 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
 
 provider "aws"{
-region = "ap-south-1"
+region = "eu-north-1"
 }
 
 resource "aws_instance" "AWSServer"{
 ami = "ami-0da59f1af71ea4ad2"
-instance_type = "t2.micro"
-key_name = "devopsmithuntechnologiesbhaskar"
-security_groups = ["launch-wizard-2"]
+instance_type = "t3.micro"
+key_name = "redhatdevops"
+security_groups = ["launch-wizard-9"]
 tags = {
 Name = "tomcatsservers"
 }
